04ff948df8aa2003e32200fef583421633ee2f2c
dan-stachlewski <dan.stachlewski@gmail.com>
2015-07-11 09:20:07 +1000

    Added the Form html

--- add-user-submit.php
+++ add-user-submit.php
@@ -3,6 +3,32 @@
 /*** BEGIN THE SESSION ***/
 session_start();
 
+/*** CHECK: USERNAME, PASSWORD & FORM TOKEN HAVE BEEN SENT ***/
+if (!isset($_POST['username'], $_POST['password'], $_POST['form_token'])) {
+	$message = 'Please enter a Valid Username and Password!';
+}
 
+/*** CHECK: FORM TOKEN IS VALID ***/
+elseif ($_POST['form_token']) {
+
+}
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Sample Login</title>
+        <link rel="stylesheet" href="css/style.css"> <link rel="author" href="humans.txt">
+    </head>
+    <body>
+	<h1>Sample Login</h1>
+	<form action="">
+		
+	</form>
+        <script src="js/main.js"></script>
+    </body>
+</html>
