c813a784ebee0514a20f2244328803b1b69a6f5f
dan-stachlewski <dan.stachlewski@gmail.com>
2015-07-11 11:52:47 +1000

    This file holds duplicate code, broken code, code that could be usefull that I don't want to delete just yet!

---
+++ code-snippets.php
@@ -0,0 +1,30 @@
+<!-- SIMPLE LOGIN FORM HTML -->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Sample Login</title>
+        <link rel="stylesheet" href="css/style.css"> <link rel="author" href="humans.txt">
+    </head>
+    <body>
+	<h1>Sample Login</h1>
+	<form action="" method="POST">
+    <ul>
+        <li>
+            <label for="username">Username</label>
+            <input id="username" name="username" type="text" value="">
+        </li>
+        <li>
+            <label for="password">Password</label>
+            <input id="password" name="password" type="text" value="">
+        </li>
+        <li>
+            <input id="login" name="login" type="submit" value="Login!">
+        </li>
+    </ul>
+	</form>
+        <script src="js/main.js"></script>
+    </body>
+</html>
